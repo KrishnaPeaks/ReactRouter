0f619ede852d6a512fb31ad705c1f1622b3b33a6
KrishnaPeaks <krishna.a3567@gmail.com>
2026-01-18 15:46:40 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,17 @@ import { BrowserRouter, createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
+import { Layout } from "./Layout";
 
-const router = createBrowserRouter([])
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>,
+    children:[{},{
+
+    }]
+  }
+])
 ReactDOM.createRoot(document.getElementById("root")).render(
 
   <RouterProvider router = {router}/>
